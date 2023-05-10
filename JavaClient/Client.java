f296c96955deb0028b518adb7dd44d2d94d1ffc0
emilysmokoff <esmokoff@uw.edu>
2023-05-10 16:48:35 -0700

    Coding changes before interoperability testing

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -6,6 +6,13 @@ import javax.xml.xpath.*;
 import org.w3c.dom.*;
 
 import org.w3c.dom.Node;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.TransformerFactory;
+import org.xml.sax.InputSource;
+
+import java.io.FileNotFoundException;
+import java.lang.UnsupportedOperationException;
 
 /**
  * This approach uses the java.net.http.HttpClient classes, which
@@ -13,8 +20,13 @@ import org.w3c.dom.Node;
  */
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    private static String host = "";
+    private static int port = 8080;
 
     public static void main(String... args) throws Exception {
+        host = args[0];
+        port = Integer.valueOf(args[1]);
+        
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -23,26 +35,410 @@ public class Client {
         System.out.println(multiply(1, 2, 3, 4, 5) == 120);
         System.out.println(divide(10, 5) == 2);
         System.out.println(modulo(10, 5) == 0);
+
+        try(Socket sock = new Socket(host, port)) {
+        }
+
     }
     public static int add(int lhs, int rhs) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("add"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < 2; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                
+                if(i == 0) {
+                    i4.appendChild(document.createTextNode(String.valueOf(lhs)));
+                    value.appendChild(i4);
+                } else {
+                    i4.appendChild(document.createTextNode(String.valueOf(rhs)));
+                    value.appendChild(i4);
+                }
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
-    public static int add(Integer... params) throws Exception {
+    public static int add(Integer... processing) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("add"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < processing.length; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                i4.appendChild(document.createTextNode(String.valueOf(processing[i])));
+                value.appendChild(i4);
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
     public static int subtract(int lhs, int rhs) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("subtract"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < 2; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                
+                if(i == 0) {
+                    i4.appendChild(document.createTextNode(String.valueOf(lhs)));
+                    value.appendChild(i4);
+                } else {
+                    i4.appendChild(document.createTextNode(String.valueOf(rhs)));
+                    value.appendChild(i4);
+                }
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
     public static int multiply(int lhs, int rhs) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("multiply"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < 2; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                
+                if(i == 0) {
+                    i4.appendChild(document.createTextNode(String.valueOf(lhs)));
+                    value.appendChild(i4);
+                } else {
+                    i4.appendChild(document.createTextNode(String.valueOf(rhs)));
+                    value.appendChild(i4);
+                }
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
-    public static int multiply(Integer... params) throws Exception {
+    public static int multiply(Integer... processing) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("multiply"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < processing.length; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                i4.appendChild(document.createTextNode(String.valueOf(processing[i])));
+                value.appendChild(i4);
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        
         return -1;
     }
     public static int divide(int lhs, int rhs) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("divide"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < 2; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                
+                if(i == 0) {
+                    i4.appendChild(document.createTextNode(String.valueOf(lhs)));
+                    value.appendChild(i4);
+                } else {
+                    i4.appendChild(document.createTextNode(String.valueOf(rhs)));
+                    value.appendChild(i4);
+                }
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
     public static int modulo(int lhs, int rhs) throws Exception {
+        try {
+            //build the xml document structure
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+
+            document.setXmlVersion("1.0");
+
+            Element root = document.createElement("methodCall");
+            document.appendChild(root);
+
+            Element methodName = document.createElement("methodName");
+            methodName.appendChild(document.createTextNode("modulo"));
+            root.appendChild(methodName);
+
+            Element params = document.createElement("params");
+            methodName.appendChild(params);
+
+            for(int i = 0; i < 2; i++) {
+                Element param = document.createElement("param");
+                params.appendChild(param);
+
+                Element value = document.createElement("value");
+                param.appendChild(value);
+
+                Element i4 = document.createElement("i4");
+                
+                if(i == 0) {
+                    i4.appendChild(document.createTextNode(String.valueOf(lhs)));
+                    value.appendChild(i4);
+                } else {
+                    i4.appendChild(document.createTextNode(String.valueOf(rhs)));
+                    value.appendChild(i4);
+                }
+
+            }
+
+            //create the xml file to be passed into the http request
+            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // DOMSource xmlSource = new DOMSource(document);
+            // StreamResult outputTarget = new StreamResult(outputStream);
+            return returnForMethods(document);
+            
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
         return -1;
     }
+
+    public static int returnForMethods (Document document) {
+        try {
+            StringWriter documentString = new StringWriter();
+            DOMSource xmlSource = new DOMSource(document);
+            StreamResult outputTarget = new StreamResult(documentString);
+            TransformerFactory.newInstance().newTransformer().transform(xmlSource, outputTarget);
+
+            //build the http request
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://" + host + ":" + String.valueOf(port) + "/RPC"))
+                .version(HttpClient.Version.HTTP_1_1)
+                .headers("User-Agent", "Emily Smokoff", "Content-Type", "text/xml")
+                .POST(HttpRequest.BodyPublishers.ofString(documentString.toString()))
+                .build();
+
+            //sending post request
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            //String responseBody = response.body();
+            String responseBody = response.body();
+
+            //prepare result to parse out
+            DocumentBuilderFactory factoryReturn = DocumentBuilderFactory.newInstance();  
+            DocumentBuilder builderReturn = factoryReturn.newDocumentBuilder(); 
+            Document docReturn = builderReturn.newDocument();
+            if(response.statusCode() != 404 && response.statusCode() != 405) { 
+                docReturn = builderReturn.parse(new InputSource(new StringReader(responseBody)));
+            } 
+
+            if(responseBody.contains("faultString") || response.statusCode() != 200) {
+                //throw exception here
+                String error="";
+                // Element root = docReturn.getDocumentElement();
+                if(responseBody.contains("faultString")) {
+                    Element root = docReturn.getDocumentElement();
+                    error = root.getElementsByTagName("faultString").item(0).getTextContent();
+                } 
+
+                if(error.equals("divide by zero")) {
+                    throw new ArithmeticException();
+                } else if (error.equals("illegal argument type")) {
+                    throw new IllegalArgumentException();
+                } else if (response.statusCode() == 404) {
+                    throw new FileNotFoundException();
+                } else if (response.statusCode() == 405) {
+                    throw new UnsupportedOperationException();
+                } else {
+                    throw new RuntimeException();
+                }
+            } else {
+                Element root = docReturn.getDocumentElement();
+                String returnResult = root.getElementsByTagName("i4").item(0).getTextContent();
+                return Integer.valueOf(returnResult);
+            }
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return -1;
+    }
+    
 }
--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -5,6 +5,24 @@ import java.util.logging.*;
 
 import static spark.Spark.*;
 
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+import java.util.*;
+import java.net.http.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+
+import org.w3c.dom.Node;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.TransformerFactory;
+import org.xml.sax.InputSource;
+
+import java.io.FileNotFoundException;
+import java.lang.UnsupportedOperationException;
+
 class Call {
     public String name;
     public List<Object> args = new ArrayList<Object>();
@@ -12,17 +30,210 @@ class Call {
 
 public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
+    private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
     public static void main(String[] args) {
-        LOG.info("Starting up on port 4567");
+        LOG.info("Starting up on port 8080");
+        port(8080);
 
-        // This is the mapping for POST requests to "/RPC";
-        // this is where you will want to handle incoming XML-RPC requests
-        post("/RPC", (request, response) -> { response.status(500); return "TBD"; });
+        //Throw an error if the path isn't RPC
+        before((request, response) -> {
+            String path = ((String) request.pathInfo()).trim();
+            if(!path.equals("/RPC")) {
+                throw new FileNotFoundException();
+            }
+        });
 
-        // Each of the verbs has a similar format: get() for GET,
-        // put() for PUT, delete() for DELETE. There's also an exception()
-        // for dealing with exceptions thrown from handlers.
-        // All of this is documented on the SparkJava website (https://sparkjava.com/).
+        //handle POST requests
+        post("/RPC", (request, response) -> { 
+            int responseStatus = 200;
+            String requestBody = request.body();
+    
+            //read the body
+            DocumentBuilderFactory requestFactory = DocumentBuilderFactory.newInstance();  
+            DocumentBuilder builderRequest = requestFactory.newDocumentBuilder();
+            Document docRequest = builderRequest.parse(new InputSource(new StringReader(requestBody)));
+
+            Call getCall = new Call();
+            //find method
+            Element root = docRequest.getDocumentElement();
+            getCall.name = root.getElementsByTagName("methodName").item(0).getTextContent();
+            //LOG.info(getCall.name);
+
+            //find integers
+            NodeList nodeList = root.getElementsByTagName("i4");
+            List<Object> splitResponse = new ArrayList<Object>();
+            
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                splitResponse.add(nodeList.item(i).getTextContent());
+            }
+            getCall.args = splitResponse;
+            //LOG.info((String) getCall.args.get(0));
+
+            Document returnDocument = xmlResults(getCall.name, getCall.args);
+
+            StringWriter documentString = new StringWriter();
+            DOMSource xmlSource = new DOMSource(returnDocument);
+            StreamResult outputTarget = new StreamResult(documentString);
+            TransformerFactory.newInstance().newTransformer().transform(xmlSource, outputTarget);
+
+            response.status(responseStatus);
+            response.type("text/xml");
+            return documentString.toString(); 
+        });
+
+        //Handle any method besides POST being put in
+        get("/RPC", (request, response) -> {
+            throw new UnsupportedOperationException();
+        });
+
+        put("/RPC", (request, response) -> {
+            throw new UnsupportedOperationException();
+        });
+
+        delete("/RPC", (request, response) -> {
+            throw new UnsupportedOperationException();
+        });
+
+        options("/RPC", (request, response) -> {
+            //throw halt(405, "Method Not Supported");
+            throw new UnsupportedOperationException();
+        });
+
+        //handle file not found exception
+        exception(FileNotFoundException.class, (exception, request, response) -> {
+            // Handle the exception here
+            response.status(404);
+
+            response.body("File Not Found");
+        });
+
+        //handle method not found error
+        exception(UnsupportedOperationException.class, (exception, request, response) -> {
+            // Handle the exception here
+            response.status(405);
+
+            response.body("Method Not Found");
+        });
+    }
+
+    public static Document xmlResults(String name, List<Object> args) throws Exception{
+        Boolean nonNum = false;
+        
+        Calc calculator = new Calc();
+
+        // DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+        Document document = docBuilder.newDocument();
+
+        document.setXmlVersion("1.0");
+
+        Element root = document.createElement("result");
+        document.appendChild(root);
+
+        int[] processingNums = new int[args.size()];
+        for(int i = 0; i < args.size(); i++) {
+            try {
+                String numToProcess = ((String) args.get(i)).trim();
+                //LOG.info("created a string out of trim");
+                int num = Integer.parseInt(numToProcess);
+                //LOG.info("set an int to the parse int");
+                processingNums[i] = num;
+                //LOG.info("made it past setting array");
+            } catch (Exception err) {
+                //LOG.info("throwing exception");
+                nonNum = true;
+            }
+        }
+        if(nonNum == false) {
+            try {
+                if(name.contains("add")) {
+                    String returnStatement = String.valueOf(calculator.add(processingNums));
+
+                    Element i4 = document.createElement("i4");
+                    i4.appendChild(document.createTextNode(returnStatement));
+                    root.appendChild(i4);
+
+                } else if (name.contains("subtract")) {
+                    String returnStatement = String.valueOf(calculator.subtract(processingNums[0], processingNums[1]));
+
+                    Element i4 = document.createElement("i4");
+                    i4.appendChild(document.createTextNode(returnStatement));
+                    root.appendChild(i4);
+
+                } else if (name.contains("divide")) {
+                    if(processingNums[1] == 0 ) {
+                        //return faultCode of 1 and faultString of "divide by zero"
+                        Element faultCode = document.createElement("faultCode");
+                        faultCode.appendChild(document.createTextNode("1"));
+                        root.appendChild(faultCode);
+
+                        Element faultString = document.createElement("faultString");
+                        faultString.appendChild(document.createTextNode("divide by zero"));
+                        root.appendChild(faultString);
+
+                    } else {
+                        String returnStatement = String.valueOf(calculator.divide(processingNums[0], processingNums[1]));
+
+                        Element i4 = document.createElement("i4");
+                        i4.appendChild(document.createTextNode(returnStatement));
+                        root.appendChild(i4);
+
+                    }
+                } else if (name.contains("multiply")) {
+                    String returnStatement = "1";
+                    if(processingNums.length == 1) {
+                        //processingNums[1] = 1;
+                        returnStatement = String.valueOf(calculator.multiply(new int[]{processingNums[0], 1}));
+                    } else if (processingNums.length > 1) {
+                        returnStatement = String.valueOf(calculator.multiply(processingNums));
+                    }
+                    
+                    Element i4 = document.createElement("i4");
+                    i4.appendChild(document.createTextNode(returnStatement));
+                    root.appendChild(i4);
+
+                } else if (name.contains("modulo")) {
+                    if(processingNums[1] == 0 ) {
+                        //return faultCode of 1 and faultString of "divide by zero"
+                        Element faultCode = document.createElement("faultCode");
+                        faultCode.appendChild(document.createTextNode("1"));
+                        root.appendChild(faultCode);
+
+                        Element faultString = document.createElement("faultString");
+                        faultString.appendChild(document.createTextNode("divide by zero"));
+                        root.appendChild(faultString);
+
+                    } else {
+                        String returnStatement = String.valueOf(calculator.modulo(processingNums[0], processingNums[1]));
+
+                        Element i4 = document.createElement("i4");
+                        i4.appendChild(document.createTextNode(returnStatement));
+                        root.appendChild(i4);
+
+                    }
+                }
+            } catch (ArithmeticException err) {
+                Element faultCode = document.createElement("faultCode");
+                faultCode.appendChild(document.createTextNode("4"));
+                root.appendChild(faultCode);
+
+                Element faultString = document.createElement("faultString");
+                faultString.appendChild(document.createTextNode("overflow error"));
+                root.appendChild(faultString);
+            }
+            
+        } else {
+            //return a faultCode of 3 and a faultString of "illegal argument type".
+            Element faultCode = document.createElement("faultCode");
+            faultCode.appendChild(document.createTextNode("3"));
+            root.appendChild(faultCode);
+
+            Element faultString = document.createElement("faultString");
+            faultString.appendChild(document.createTextNode("illegal argument type"));
+            root.appendChild(faultString);
+        }
+        
+        return document;
     }
 }
